2ea44328b15e0817acd82c75751bbeb5f16a83ae
Dustin L Sears <dlsears2789@yahoo.com>
2015-10-29 21:18:56 -0400

    updates to Results page. changed app ID on index.html

--- handlers/Results/index.js
+++ handlers/Results/index.js
@@ -9,10 +9,12 @@ class Results extends React.Component {
   render(): ?ReactElement {
     return (
       <div className="Results">
-        <img src="/public/img/sample-circle-photo.png" />
+        <img src="/public/img/sample-circle-photo.png" className="img-resize" />
         <h1>Career Category</h1>
         <h4>This is where your tagline goes</h4>
-        <p className="results-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, ante rutrum posuere, tortor ipsum semper tortor, non rhoncus sem eros ut nisl. Nunc et aliquet lacus, ac congue neque. Vestibulum diam augue, iaculis quis egestas cursus, iaculis a est. Pellentesque id commodo lorem. Cras nec vestibulum tellus. Nunc congue sapien nec venenatis consectetur. Ut a nunc id massa dignissim elementum at at felis.!</p>
+
+        <div className="Results-content">
+        <p className="text-black">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, ante rutrum posuere, tortor ipsum semper tortor, non rhoncus sem eros ut nisl. Nunc et aliquet lacus, ac congue neque. Vestibulum diam augue, iaculis quis egestas cursus, iaculis a est. Pellentesque id commodo lorem. Cras nec vestibulum tellus. Nunc congue sapien nec venenatis consectetur. Ut a nunc id massa dignissim elementum at at felis.</p></div>
       </div>
     );
   }
